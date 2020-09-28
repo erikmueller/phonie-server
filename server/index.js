eeaafed9b86b000a1d5dc0c6810ea72c5bb90b7f
Erik Müller <emueller@newstore.com>
2020-09-28 21:06:27 +0200

    Add build pipeline

--- server/index.js
+++ server/index.js
@@ -3,4 +3,4 @@ const serveStatic = require('serve-static')
 
 const { apiRouter } = require('./router')
 
-app.use('/api', apiRouter).use('/*', serveStatic('public')).listen(6699)
+app.use('/api', apiRouter).use(serveStatic('public')).listen(6699)
---
+++ src/components/App.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const App = () => {
+  return <div>This is some text</div>
+}
+
+export default App
